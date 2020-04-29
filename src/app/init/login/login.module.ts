4fe0eac118e2fd67f0cb15eb88de177ce37a0b62
Emmanuel Ortega <kenpomuaythai@gmail.com>
2020-04-28 20:38:48 -0400

    configuracion de rutas en el modulo login

--- src/app/init/login/login.module.ts
+++ src/app/init/login/login.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { IndexComponent } from './index/index.component';
+import { LoginRoutingModule } from './login-routing.module';
 
+// enrutamiento
 
 
 @NgModule({
-  declarations: [LoginComponent, IndexComponent],
+  declarations: [
+    LoginComponent, 
+    IndexComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule
   ]
 })
 export class LoginModule { }
