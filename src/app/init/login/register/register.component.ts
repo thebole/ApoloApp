57f576e872ed2b263587f20ce11ea63ff7ba2f79
Emmanuel Ortega <kenpomuaythai@gmail.com>
2020-05-02 10:39:01 -0400

    agregando logica para registrar usuario

--- src/app/init/login/register/register.component.ts
+++ src/app/init/login/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/shared/services/login/authentication.service';
+import { UserService } from 'src/app/shared/services/login/user.service';
+import { AlertService } from 'src/app/shared/services/alert.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +14,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private alertService: AlertService) { 
+      if (this.authenticationService.currentUserValue) {
+        this.router.navigate(['/']);
+    }
+    }
 
   ngOnInit(): void {
     this.buildRegisterForm();
   }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
 
   buildRegisterForm(): FormGroup {
     return this.registerForm = this.fb.group({
@@ -29,5 +47,28 @@ export class RegisterComponent implements OnInit {
 
   registerSubmit() {
     console.log(this.registerForm.value);
+
+    this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.userService.register(this.registerForm.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
   }
 }
