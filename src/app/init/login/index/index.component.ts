e9730561142f3d949ba60749d270ec30217b92f5
Emmanuel Ortega <kenpomuaythai@gmail.com>
2020-05-10 15:56:50 -0400

    cambio temporal del index, se cambio el index para hacer una breve demostracion

--- src/app/init/login/index/index.component.ts
+++ src/app/init/login/index/index.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/shared/models/User';
 import { AuthenticationService } from 'src/app/shared/services/login/authentication.service';
 import { UserService } from 'src/app/shared/services/login/user.service';
 import { first } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -12,18 +13,26 @@ import { first } from 'rxjs/operators';
 export class IndexComponent implements OnInit {
   currentUser: User;
   users = [];
+  user: string;
   constructor(
     private authenticationService: AuthenticationService,
-    private userService: UserService
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.loadUsers();
+    this.user = localStorage.getItem('name');
   }
 
   private loadUsers() {
     // this.userService.getAll().pipe(first()).subscribe(users => { this.users = users;});
     console.log('soy el index!!!!!');
   }
+
+  logout () {
+    this.authenticationService.logout();
+    this.router.navigateByUrl('/login');
+  }
+  
 
 }
